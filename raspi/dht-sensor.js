44f4cd4544b112af55518239042b3136957afa9b
Haakasaurus <kramerhjohnson@gmail.com>
2020-01-28 11:21:25 -0800

    Added more comments and clarifications to dht-sensor.js

--- raspi/dht-sensor.js
+++ raspi/dht-sensor.js
@@ -8,16 +8,18 @@ function readSensor() {
     sensor.setMaxRetries(10);
     sensor.initialize(11, 17);
 
-	// Read from the sensor and log the output.
+    // Read from the sensor and log the output.
+    // 11 is the sensor type (dht11) and 17 is the GPIO port number.
     sensor.read(11, 17)
-        .then( res => {
-            const tempF = res.temperature.toFixed(1) * 1.8 + 32; 
+        .then(res => {
+            const tempF = res.temperature.toFixed(1) * 1.8 + 32;
+            const humidity = res.humidity.toFixed(1);
             console.log(`temp: ${tempF} degrees F, ` + 
-                `humidity: ${res.humidity.toFixed(1)}% ` +
+                `humidity: ${humidity}% ` +
                 `at ${new Date()}.`
 	    );
         })
-	.catch(err => console.log(`There was an error: ${err}`));
+	.catch(err => console.log(`Could not read sensor data: ${err}`));
 };
 
 module.exports = readSensor();
