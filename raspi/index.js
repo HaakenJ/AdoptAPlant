0d376d6503326c3c7de829b783712116ff90d5a4
Haakasaurus <kramerhjohnson@gmail.com>
2020-02-01 12:04:09 -0800

    Added routes for populating table and pump button on test html

--- raspi/index.js
+++ raspi/index.js
@@ -1,5 +1,22 @@
 $(document).ready(() => {
     $.get("/api/templog", data => {
-        
+        data.forEach(logObj => {
+            let newRow = $("<tr>");
+            Object.keys(logObj).forEach(dataKey => {
+                let newData = $("<td>").text(logObj[dataKey]);
+                newRow.append(newData);
+            })
+            $("#temp-table").append(newRow);
+        });
+    });
+
+    $("#water-btn").on("click", () => {
+        $.ajax({
+            url: "/api/water",
+            type: "PUT",
+            success: res => {
+                console.log(res);
+            }
+        });
     })
-})
+});
--- raspi/pump.js
+++ raspi/pump.js
@@ -1,16 +1,10 @@
 const Gpio = require("onoff").Gpio;
+// Use GPIO pin 27 and specify that it is output.
+const pump = new Gpio(27, "out");
 
 // Function to toggle the running state of the water pump.
 module.exports = function togglepump() {
-    // Check if GPIO pins are accessible from current environment.
-    if (Gpio.accessible) {
-        // Use GPIO pin 27 and specify that it is output.
-        const pump = new Gpio(27, "out");
-        pump.read()
-            .then(value => pump.write(value ^ 1))
-            .catch(err => console.log(err));
-    } else {
-        console.log("No GPIO pins are currently available.  " + 
-        "Please ensure that this code is running on a raspberryPi.");
-    }
+    pump.read()
+        .then(value => pump.write(value ^ 1))
+        .catch(err => console.log(err));
 };
