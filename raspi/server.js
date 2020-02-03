41a835613f0a9f0137a60baac9b37f6d49ea4a5d
HaakenJ <kramerhjohnson@gmail.com>
2020-02-02 20:07:41 -0800

    Updated all files to use new port forwarding address.

--- raspi/server.js
+++ raspi/server.js
@@ -6,7 +6,7 @@ const pump = require("./pump");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8888;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
