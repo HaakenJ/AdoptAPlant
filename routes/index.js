3adcc725206f6410472908ff1a0087826680282a
Haakasaurus <kramerhjohnson@gmail.com>
2020-02-05 21:05:04 -0800

    Added pump, light, and dht controller modules to controller dir, moved piRoutes to own file, included piRoutes in index.js route

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,7 @@
 const router = require("express").Router();
-const fs = require("fs");
-const csv = require("csv-parser");
-const pump = require("./pump");
-const setLightState = require("./light");
+const bookRoutes = require("./piRoutes");
 
-router.get("/api/templog", (req, res) => {
-    let results = [];
-    fs.createReadStream("raspi/templog.csv")
-    .pipe(csv())
-    .on("data", data => results.push(data))
-    .on("end", () => {
-        results = results.slice(results.length - 5,);
-        return res.send(results);
-    })
-})
-
-router.put("/api/water", (req, res) => {
-    pump.runPump();
-    console.log("Pump is on");
-    setTimeout(() => {
-        pump.shutOffPump();
-        console.log("pump is off");
-    }, 3000);
-})
-
-router.put("/api/light", (req, res) => {
-    setLightState(req.body.power);
-    res.end();
-})
+// Raspberry Pi Routes
+router.use("/pi", bookRoutes);
 
 module.exports = router;
