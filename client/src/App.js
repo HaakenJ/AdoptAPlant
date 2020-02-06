f14453fcf90229c569796363d460b7a1cd123bfa
Surenkhuu Shagdarsuren <surenkhuu@gmail.com>
2020-02-05 20:43:12 -0800

    login and signup added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,60 @@
-import React from "react";
-import Search from "./pages/Search";
-import Saved from "./pages/Saved";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
 
 function App() {
-  return ();
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>HOME</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
 }
 
 export default App;
+
+
+
+
+
+
+// import React from "react";
+// import Search from "./pages/Search";
+// import Saved from "./pages/Saved";
+// import NoMatch from "./pages/NoMatch";
+// import Nav from "./components/Nav";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// function App() {
+//   return ();
+// }
+
+// export default App;
