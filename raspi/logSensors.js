804f819bc22b6fe7aed7a7422587c6977c33e207
Haakasaurus <kramerhjohnson@gmail.com>
2020-02-07 10:47:42 -0800

    Created logSensors script to log soil data to database.  This will run in crontab and has not been tested.  Needed real model and db.

--- raspi/logSensors.js
+++ raspi/logSensors.js
@@ -1,2 +1,27 @@
 const readFromDHT = require("./dht-sensor");
 const readSoilSensor = require("./soilSensor");
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 
+    "mongodb://localhost/temphumiditylog" ||
+    // Not sure if this is the correct address.
+    "mongodb://AdoptAPlant.hopto.org:8888/temphumiditylog");
+
+
+readFromDHT()
+.then((temp, humidity) => {
+    readSoilSensor()
+    .then(soilHumidity => {
+        const dataObj = {
+            "temp": temp,
+            "humidity": humidity,
+            "soilHumidity": soilHumidity
+        };
+        db.Temp
+          .create(dataObj)
+          .then(dbModel => console.log(dbModel))
+          .catch(err => console.log(err));
+    })
+})
