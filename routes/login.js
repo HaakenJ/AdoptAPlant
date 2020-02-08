f03fa83a68c2695e72b7ee1159905bf5cfac5cf2
Haakasaurus <kramerhjohnson@gmail.com>
2020-02-08 13:39:24 -0800

    Wrote login route for passport

--- routes/login.js
+++ routes/login.js
@@ -4,8 +4,18 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const passport = require('../config/passport');
 
-router.use('/login', (req, res) => {
-  console.log("")
+router.use('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: 'landing',
+    failureFlash: 'Invalid username or password'
+  }),
+  (req, res) => {
+    console.log(`${req.user} is logged in.`);
+    const userInfo = {
+      username: req.user.username
+    };
+    res.send(userInfo);
+  }
 });
 
 module.exports = router;
