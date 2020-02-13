62f59ea7db698b29c5cb1f7b0dffcd5951c451e8
Kyle LaMont <kylerlamont@gmail.com>
2020-02-12 19:59:10 -0800

    added guest access for landing page for demo day/ recruiters

--- client/src/components/PlantStream/index.js
+++ client/src/components/PlantStream/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export function PlantStream(props) {
     return(
-        <img src={"http://adoptaplant.hopto.org:8081/"} style={{ height: "100%", width: "100%", marginBottom: "25px" }} />
+        <img src={"http://adoptaplant.hopto.org:8081/"} alt="plogo" style={{ height: "100%", width: "100%", marginBottom: "25px" }} />
     );
 }
 
