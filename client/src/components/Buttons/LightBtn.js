70323a58a233f18f50e6608513fe897f59b3d75b
Kyle LaMont <kylerlamont@gmail.com>
2020-02-12 20:37:37 -0800

    added {...props} to button and kept on click function

--- client/src/components/Buttons/LightBtn.js
+++ client/src/components/Buttons/LightBtn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export function LightBtn(props) {
     return (
-        <button type="submit" className=" btn btn-primary" id="light-btn" style={{ backgroundColor: "yellow", color:"black" }}>{props.children}</button >
+        <button type="submit" {...props} className=" btn btn-primary" id="light-btn" style={{ backgroundColor: "yellow", color:"black" }}>{props.children}</button >
     );
 }
 
--- client/src/components/Buttons/SubmitBtn.js
+++ client/src/components/Buttons/SubmitBtn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export function SubmitBtn(props) {
     return (
-        <button type="submit" className="btn btn-primary btn-flex">{props.children}</button>
+        <button type="submit" {...props} className="btn btn-primary btn-flex">{props.children}</button>
     );
 }
 
