bcffca308e577b253c0f1e8c4aa6f34df7a24a09
Haakasaurus <kramerhjohnson@gmail.com>
2020-02-12 18:40:08 -0800

    Added light put route to routes/

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 // const apiRoutes = require("./api");
 const signupRoute = require("./signup");
 const loginRoute = require("./login");
+const lightRoute = require("./light");
 
 // API Routes
 // router.use("/api", apiRoutes);
@@ -10,6 +11,7 @@ const loginRoute = require("./login");
 //HTML Routes
 router.use(signupRoute);
 router.use(loginRoute);
+router.use(lightRoute);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
