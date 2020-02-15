4d129a6398d03fbd6b462b56e826c3a11305de29
Kyle LaMont <kylerlamont@gmail.com>
2020-02-15 11:15:12 -0800

    removed unused links from pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Nav from './components/Nav'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserLogin from './pages/login.js'
 import SignUp from "./pages/signup.js";
 import Welcome from './pages/welcome.js';
--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { relative } from 'path';
 import PlantStream from '../components/PlantStream'
 import WaterBtn from '../components/Buttons/WaterBtn'
 import LightBtn from '../components/Buttons/LightBtn'
