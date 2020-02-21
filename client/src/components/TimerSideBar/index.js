2c5b57d57771d04d65c06966383b292cf8e0a251
Haakasaurus <kramerhjohnson@gmail.com>
2020-02-21 10:03:36 -0800

    Combined dropdown into TimerSideBar, Styled timersidebar, modified functions to call getTimerState each time the state is updated.

--- client/src/components/TimerSideBar/index.js
+++ client/src/components/TimerSideBar/index.js
@@ -6,8 +6,8 @@ class TimerSideBar extends Component {
     constructor() {
         super();
         this.state = {
-            isJobRunning = null,
-            time = null
+            isJobRunning: null,
+            time: null
         }
     }
 
@@ -15,12 +15,12 @@ class TimerSideBar extends Component {
         this.getTimerState();
     }
 
-    getTimerState() {
+    getTimerState = () => {
         API.getTimeAndJobStatus()
         .then(response => {
             this.setState({
                 isJobRunning: response.data.runJob,
-                time: response.data.time
+                time: response.data.waterAt
             })
         })
         .catch(err => {
@@ -29,10 +29,11 @@ class TimerSideBar extends Component {
         })
     }
 
-    startJob() {
+    startJob = () => {
         API.startJob()
         .then(response => {
             console.log("The watering schedule has been started");
+            this.getTimerState();
         })
         .catch(err => {
             console.log(`There was an error updating runJob 
@@ -40,14 +41,23 @@ class TimerSideBar extends Component {
         })
     }
 
-    stopJob() {
+    stopJob = () => {
         API.stopJob()
         .then(response => {
             console.log("The watering schedule has been stopped");
+            this.getTimerState();
         })
         .catch(err => {
             console.log(`There was an error updating runJob 
             on firebase: ${err}`);
+        })
+    }
+
+    handleDropdownClick = (event) => {
+        API.setTime(event.target.dataset.time)
+        .then(response => {
+            console.log(response);
+            this.getTimerState();
         })
     }
 
@@ -56,19 +66,55 @@ class TimerSideBar extends Component {
             <div className="auth-inner temp-container col col-sm-3" id="ContainerRight">
                 {
                     this.state.isJobRunning ?
-                        <div>
-                            <button type="button" class="btn btn-dark" onClick={this.stopJob} >Cancel Schedule</button>
-                            <p>Your plant is set to be watered every day at: </p>
-                            <p>{this.state.time}</p>
+                        <div className="mb-5">
+                            <button type="button" className="btn btn-dark mb-5" onClick={this.stopJob} >Cancel Schedule</button>
+                            <p className="timer-header">Your plant is set to be watered every day at: </p>
+                            <p>{this.state.time}:00</p>
                         </div> 
                         :
-                        <div>
-                            <button type="button" class="btn btn-light" onClick={this.startJob} >Start Schedule</button>
-                            <p>Your water schedule is not currently set.</p>
+                        <div className="mb-5">
+                            <button type="button" className="btn btn-light mb-5" onClick={this.startJob} >Start Schedule</button>
+                            <p className="timer-header">Your plant will be watered every day at: </p>
+                            <p>{this.state.time}:00</p>
+                            <p className="timer-header">Press 'Start Schedule' to start the timer!</p>
                         </div>
                 }
-                <Dropdown />                           
+                
+                <div className="dropdown show">
+                    <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Choose a time
+                    </a>
+
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="1" href="#">1:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="2" href="#">2:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="3" href="#">3:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="4" href="#">4:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="5" href="#">5:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="6" href="#">6:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="7" href="#">7:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="8" href="#">8:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="9" href="#">9:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="10" href="#">10:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="11" href="#">11:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="12" href="#">12:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="13" href="#">13:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="14" href="#">14:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="15" href="#">15:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="16" href="#">16:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="17" href="#">17:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="18" href="#">18:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="19" href="#">19:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="20" href="#">20:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="21" href="#">21:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="22" href="#">22:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="23" href="#">23:00</a>
+                        <a className="dropdown-item" onClick={this.handleDropdownClick} data-time="0" href="#">00:00</a>
+                    </div>
+                </div>
             </div>
         )
     }
 }
+
+export default TimerSideBar;
