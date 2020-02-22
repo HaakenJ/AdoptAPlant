edbeb1b7b3efeb74aec65176d3c1b029369cf904
Haakasaurus <kramerhjohnson@gmail.com>
2020-02-21 16:16:22 -0800

    Altered colors of light on and off buttons

--- client/src/components/Buttons/LightOnBtn.js
+++ client/src/components/Buttons/LightOnBtn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export function LightOnBtn(props) {
     return (
-        <button type="submit" {...props} className=" btn btn-primary" id="light-btn" style={{ backgroundColor: "#FFFBCE", color:"#001D00" }}>{props.children}</button >
+        <button type="submit" {...props} className=" btn btn-primary" id="light-btn" style={{ backgroundColor: "rgb(255, 255, 245)", color:"#001D00" }}>{props.children}</button >
     );
 }
 
