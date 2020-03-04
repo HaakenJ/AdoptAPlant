fd59091957790809f5892b9976f51dd48070b851
Haakasaurus <kramerhjohnson@gmail.com>
2020-03-03 19:29:30 -0800

    waterTimer changes

--- controllers/waterTimer.js
+++ controllers/waterTimer.js
@@ -4,16 +4,16 @@ const CronJob = require('cron').CronJob;
 // const setPumpState = require('../controllers/pump');
 
 firebase.initializeApp({
-    appName: "adoptaplant",
-    serviceAccount: "../service-account.json",
-    authDomain: "adoptaplant-1b4e1.firebaseapp.com",
-    databaseURL: "https://adoptaplant-1b4e1.firebaseio.com",
-    storageBucket: "adoptaplant-1b4e1.appspot.com"
+    appName: 'adoptaplant',
+    serviceAccount: '../service-account.json',
+    authDomain: 'adoptaplant-1b4e1.firebaseapp.com',
+    databaseURL: 'https://adoptaplant-1b4e1.firebaseio.com',
+    storageBucket: 'adoptaplant-1b4e1.appspot.com'
 });
 
 const db = firebase.app().database();
 
-const ref = db.ref("commands/time");
+const ref = db.ref('commands/time');
 
 
 
@@ -24,7 +24,7 @@ const waterJob = new CronJob('* * * * * *', () => {
     setPumpState('on')
         .then(() => {
             setTimeout(() => {
-                setPumpState("off");
+                setPumpState('off');
                 ref.update({
                         water: false
                     })
@@ -46,34 +46,34 @@ const waterJob = new CronJob('* * * * * *', () => {
 }, null, false, 'America/Los_Angeles');
 
 // Listen for changes to the database.
-ref.on("value", snap => {
+ref.on('value', snap => {
     // If waterTime is set to false rather than a time, stop the job.
     if (!snap.val().runJob) {
-        console.log("Stopping job");
+        console.log('Stopping job');
         waterJob.stop();
     } else {
         let newCronTime = ``;
-        console.log("Starting job");
+        console.log('Starting job');
         console.log(snap.val().waterAt);
         console.log(snap.val().frequency);
         // newCronTime = cronTime.everyDayAt(snap.val().waterAt);
         switch (snap.val().frequency) {
-            case "Every Day":
+            case 'Every Day':
                 newCronTime = `0 ${snap.val().waterAt} * * *`;
                 break;
-            case "Every Other Day":
+            case 'Every Other Day':
                 newCronTime = `0 ${snap.val().waterAt} */2 * *`;
                 break;
-            case "Every Three Days":
+            case 'Every Three Days':
                 newCronTime = `0 ${snap.val().waterAt} */3 * *`;
                 break;
-            case "Every Four Days":
+            case 'Every Four Days':
                 newCronTime = `0 ${snap.val().waterAt} */4 * *`;
                 break;
-            case "Every Five Days":
+            case 'Every Five Days':
                 newCronTime = `0 ${snap.val().waterAt} */5 * *`;
                 break;
-            case "Every Six Days":
+            case 'Every Six Days':
                 newCronTime = `0 ${snap.val().waterAt} */6 * *`;
                 break;
             default:
@@ -82,6 +82,7 @@ ref.on("value", snap => {
         console.log(newCronTime);
         newCronTime = '* * * * *';
         const time = new CronTime(newCronTime);
+        console.log(time);
         waterJob.setTime(time);
         waterJob.start();
     }
